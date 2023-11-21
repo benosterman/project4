bf0f0dd7c0b9205770633932bb29ec4216ba0e4b
noahbweiss <nbweiss6@gmail.com>
2023-11-21 15:01:26 -0800

    Update Actor.h
    first edit test

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -5,4 +5,11 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class Actor : GraphObject {
+public:
+    Actor(int ID, int x, int y, Direction dir, float size, unsigned int depth) : GraphObject(ID, x, y, dir, size, depth){}
+    void virtual doSomething() = 0;
+};
+
+
 #endif // ACTOR_H_
