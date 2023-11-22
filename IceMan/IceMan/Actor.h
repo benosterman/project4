bf4e49d4f91d4979360c682e07f49240b36d6dca
benosterman <46685163+benosterman@users.noreply.github.com>
2023-11-22 11:41:42 -0800

    Update Actor.h

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -4,12 +4,30 @@
 #include "GraphObject.h"
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class StudentWorld;
+class Actor : public GraphObject
 
-class Actor : GraphObject {
+{
+private:
+StudentWorld* myWorld;
+bool isAlive;
 public:
-    Actor(int ID, int x, int y, Direction dir, float size, unsigned int depth) : GraphObject(ID, x, y, dir, size, depth){}
-    void virtual doSomething() = 0;
+    
+    Actor(StudentWorld* world, int imageID, double startX, double startY, Direction dir = right, int depth = 0) : GraphObject(imageID, startX, startY), myWorld(world), isAlive(true) {}
+    
+    virtual ~Actor() {}
+    
 };
 
 
+class Iceman : public Actor
+{
+public: 
+    Iceman(StudentWorld* world, int startX, int startY) : Actor(world, IID_PLAYER, startX, startY, right, 1)
+    {
+        setVisible(true);
+        
+    }
+    
+};
 #endif // ACTOR_H_
